de5be5ac04c8271d9a3e2a9ab9605e74f794d7c5
chaeyeon <321codus@naver.com>
2022-04-26 19:48:22 +0900

    Fix ArtClick to do not be able to click when popup is activing
    - 팝업이 활성화 상태일 때 갤러리 내 아트 클릭 불가

--- Assets/Scripts/MyRoom/Slot.cs
+++ Assets/Scripts/MyRoom/Slot.cs
@@ -5,19 +5,27 @@ using UnityEngine.UI;
 
 public class Slot : MonoBehaviour
 {
+    GameObject[] framelist;
     public GameObject frame;
     public Button slot;
     public RawImage img;
     public Texture art;
     void Start()
     {
-        //        frame = GameObject.FindGameObjectsWithTag("MyRoomFrame");      //myroom 내 frame의 배열
-        
+//        framelist = GameObject.FindGameObjectsWithTag("MyRoomFrame");      //myroom 내 frame의 배열       
         slot.onClick.AddListener(OnClick_Slot);
     }
 
     public void OnClick_Slot()
     {
+        //if ("Slot".Equals(gameObject.name))
+        //{
+
+        //}
+        //else
+        //{
+            
+        //}
         frame = GameObject.Find("MyRoom_Frame1");
         art = img.texture;
         
