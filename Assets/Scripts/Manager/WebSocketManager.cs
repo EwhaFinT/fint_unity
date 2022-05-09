ce05790704a1602cfe14a1743e1ef84a073d89f9
KJI-DEC <kji_dec22@ewhain.net>
2022-05-09 15:17:36 +0900

    Add: remote player 보이는 기능 추가

--- Assets/Scripts/Manager/WebSocketManager.cs
+++ Assets/Scripts/Manager/WebSocketManager.cs
@@ -12,12 +12,12 @@ public class WebSocketManager : MonoBehaviour
     public GameObject player;
     public List<GameObject> playerPrefab;
     private Dictionary<string, GameObject> remoteplayer = new Dictionary<string, GameObject>();
-    private string playerId = "me";
+    private string playerId = "test";
     private int playerCharacter = 1;
-    private string playerCommunity = "communityId";
+    private string playerCommunity = "communityId"; // TODO: communityId, userId 받아오는 함수 호출 필요 (start 함수 내)
     private PositionData playerData;
 
-    void Start()
+    public void Start()
     {
         webSocket = new WebSocket("ws://fintribesocket.herokuapp.com");
         webSocket.Connect();
@@ -33,7 +33,7 @@ public class WebSocketManager : MonoBehaviour
                     _actions.Enqueue(() => CreateRemotePlayer(playerData.userId, playerData.character, playerData.position, playerData.communityId));
                     break;
                 case PositionData.Command.Update:
-                    _actions.Enqueue(() => MoveRemotePlayer(playerData.userId, playerData.position, playerData.communityId));
+                    _actions.Enqueue(() => MoveRemotePlayer(playerData.userId, playerData.position, playerData.communityId, playerData.character));
                     break;
                 case PositionData.Command.Delete:
                     DeleteRemotePlayer(playerData.userId);
@@ -47,7 +47,8 @@ public class WebSocketManager : MonoBehaviour
 
     private double timeDelta = 0;
 
-    private void Update()
+
+    public void Update()
     {
         // Work the dispatched actions on the Unity main thread
         while (_actions.Count > 0)
@@ -59,7 +60,7 @@ public class WebSocketManager : MonoBehaviour
         }
     }
 
-    void FixedUpdate()
+    public void FixedUpdate()
     {
         timeDelta += Time.deltaTime;
         if (timeDelta > 10 / 60)
@@ -79,27 +80,39 @@ public class WebSocketManager : MonoBehaviour
 
     private void CreateRemotePlayer(string userId, int character, Vector3 position, string communityId)
     {
-        if (!userId.Equals(playerId))
+        if (!userId.Equals(playerId) && !remoteplayer.ContainsKey(userId))
         {
-            GameObject remotePlayer = Instantiate(playerPrefab[character], position, Quaternion.identity);
-            remoteplayer.Add(userId, remotePlayer);
-            if ((position.y == 13 && !playerCommunity.Equals(communityId)) || (position.y == 26))
+            remoteplayer.Add(userId, Instantiate(playerPrefab[character], position, Quaternion.identity));
+            if ((position.y >= 13 && !playerCommunity.Equals(communityId)) || (position.y >= 26))
             {
-                remotePlayer.SetActive(false);
+                Debug.Log("false activate");
+                remoteplayer[userId].SetActive(false);
             }
             else
             {
-                remotePlayer.SetActive(true);
+                Debug.Log("activate");
+                remoteplayer[userId].SetActive(true);
             }
+        }
+        else if (remoteplayer.ContainsKey(userId))
+        {
+            MoveRemotePlayer(userId, position, communityId, character);
         }
     }
 
-    private void MoveRemotePlayer(string userId, Vector3 position, string communityId)
+    private void MoveRemotePlayer(string userId, Vector3 position, string communityId, int character)
     {
         if (!userId.Equals(playerId))
         {
+            if (!remoteplayer.ContainsKey(userId))
+            {
+                GameObject remotePlayer = Instantiate(playerPrefab[character], position, Quaternion.identity);
+                Debug.Log(remotePlayer);
+                remoteplayer.Add(userId, remotePlayer);
+            }
             remoteplayer[userId].transform.position = position;
-            if ((position.y == 13 && !playerCommunity.Equals(communityId)) || (position.y == 26))
+            Debug.Log("position updated");
+            if ((position.y >= 13 && !playerCommunity.Equals(communityId)) || (position.y >= 26))
             {
                 remoteplayer[userId].SetActive(false);
             }
@@ -117,7 +130,7 @@ public class WebSocketManager : MonoBehaviour
     }
 }
 
-[System.Serializable]
+[Serializable]
 public class PositionData
 {
     public PositionData(Vector3 position, int character, string userId, string communityId, Command command)
