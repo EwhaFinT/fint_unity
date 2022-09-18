cd1931510e52a3ae2db0f56475a06953ce78ef89
chaeyeon <321codus@naver.com>
2022-09-18 15:07:12 +0900

    Add AR item

--- Assets/Scripts/Gallery/ArtPanelAr.cs
+++ Assets/Scripts/Gallery/ArtPanelAr.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 using UnityEngine.UI;
 public class ArtPanelAr : MonoBehaviour
 {
-    public Button btn_close, btn_auction;
-    public GameObject artPanel;
+    public Button btn_close, btn_auction, btn_Qrclose, qrcode;
+    public GameObject artPanel, qrPanel;
     // Start is called before the first frame update
     void Start()
     {
+        qrcode.onClick.AddListener(Onclicked_show);
         btn_close.onClick.AddListener(Onclicked_close);
+        btn_Qrclose.onClick.AddListener(Onclicked_Qrclose);
         btn_auction.onClick.AddListener(Onclicked_auction);
     }
 
@@ -32,6 +34,11 @@ public class ArtPanelAr : MonoBehaviour
     public void Onclicked_close()
     {
         artPanel.SetActive(false);
+        qrPanel.SetActive(false);
+    }
+    public void Onclicked_Qrclose()
+    {
+        qrPanel.SetActive(false);
     }
 
     public void Onclicked_auction()
@@ -41,4 +48,8 @@ public class ArtPanelAr : MonoBehaviour
         auctionPanel.panelStart();
     }
 
+    public void Onclicked_show()
+    {
+        qrPanel.SetActive(true);
+    }
 }
